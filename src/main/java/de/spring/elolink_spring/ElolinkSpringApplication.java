06ab21ae700d2dbdc651c383451cba572cc5a8a7
EloStein <steinole@yahoo.com>
2026-01-11 22:29:37 +0100

    First Test

--- src/main/java/de/spring/elolink_spring/ElolinkSpringApplication.java
+++ src/main/java/de/spring/elolink_spring/ElolinkSpringApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ElolinkSpringApplication {
 
+	public void serverTest(){
+		System.out.println("This is the Server");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ElolinkSpringApplication.class, args);
 	}
